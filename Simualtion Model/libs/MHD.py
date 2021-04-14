ab96bd2854a059692d69d4a9ab4f1904d0c6d99e
magdamd <49106367+magdamd@users.noreply.github.com>
2021-04-14 15:34:35 +0100

    Update MHD.py

--- Simualtion Model/libs/MHD.py
+++ Simualtion Model/libs/MHD.py
@@ -1,8 +1,15 @@
 import numpy as np
+import magnetic_fields as mf
+import funtions_and_constants as func
+from functions import CONSTANTS as CONST
 
 class Plasma:
-    def __init__(self, NX,NY,NZ, DX,DY,DZ, rho):
-        # Read the file with initial values of the plasma fluid
+    '''
+        Plasma object that is described as a MHD ideal fluid
+    '''
+    def __init__(self,filename,initial_density, initial_u, inital_v, initial_w, initial_presure, initial_force, magnetic_sources):
+        with open(filename,'r') as f:
+            # Read the file with initial values of the plasma fluid
             line = f.readline()
             while line:
                 tag,value = line.split(':')
@@ -13,94 +20,30 @@ class Plasma:
                     self.__XMIN, self.__XMAX, self.__YMIN, self.__YMAX = [float(v) for v in value.split(',')] # Dimentions of the box
                     self.__DX = (self.__XMAX-self.__XMIN)/(self.__NX-1)
                     self.__DY = (self.__YMAX-self.__YMIN)/(self.__NY-1)
+                    X = np.linspace(self.__XMIN-self.__DX,self.__XMAX+self.__DX,self.__NX+2)
+                    Z = np.linspace(self.__YMIN-self.__DY,self.__YMAX+self.__DY,self.__NY+2)
+                    self.__POS = np.array([(x,0,z) for z in Z for x in X])
                 elif tag == 'Temporal':
                     self.__DT, self.__TF = [float(v) for v in value.split(',')]
                 line = f.readline()
-            self.__RHO = np.array(initial_density) # Mass density initial condition
-            self.__U = np.array(initial_u) # Initial velocity in XX
-            self.__V = np.array(initial_v) # Initial velocity in YY
-            self.__P = np.array(initial_presure) #Intial Presure matrix
-            self.__F = np.array(initial_Force) #Intial Force matrix
-        
-"""
-class TheSolver3D:
-    def back_diff1st_3d (self, f, coeff1 , coeff2, coeff3):
-        """ Calcula a 'derivada para trás' 3D de um campo f, devolvendo como output o campo resultante g"""
-        g = coeff1 ∗(f[ 1:−1 , 1:−1 , 1:−1 ] − f[:−2,1:−1, 1:-1]) / self.__DX
-        g += coeff2 ∗(f[ 1:−1 , 1:−1 , 1:−1 ] − f[ 1:−1 ,:−2, 1:-1]) / self.__DY
-        g += coeff3 ∗(f[ 1:−1 , 1:−1 , 1:−1 ] − f[ 1:−1 , 1:-1,:−2]) / self.__DZ
-        return g
-    
-    def central_diff_implicit_1st_3d (self, f, coeff1, coeff2, coeff3):
-        """ Calcula a 'centrada' 3D de um campo f, devolvendo como output o campo resultante g"""
-        g = coeff1*( f[2:,1:-1,1:-1] + f [:-2,1:-1,1:-1])/(2*self.__DX)
-        g += coeff2*( f[1:-1,2:,1:-1] + f[1:-1,:-2,1:-1])/(2*self.__DY)
-        g += coeff3*( f[1:-1,1:-1,2:] + f[1:-1,1:-1,:-2])/(2*self.__DZ)
-        return g
-    
-    def central_diff_implicit_1st_3d (self, f, coeff1, coeff2, coeff3):
-        """ Calcula a 'centrada' 3D de um campo f, devolvendo como output o campo resultante g"""
-        g = coeff1*( f[2:,1:-1,1:-1] + f [:-2,1:-1,1:-1])/(2*self.__DX)
-        g += coeff2*( f[1:-1,2:,1:-1] + f[1:-1,:-2,1:-1])/(2*self.__DY)
-        g += coeff3*( f[1:-1,1:-1,2:] + f[1:-1,1:-1,:-2])/(2*self.__DZ)
-        return g
-    
-    def central_diff_1st_3dX (self, f, coeff) :
-        """ Calcula a "derivada centrada" da componente X de um campo 3D f"""
-        g = coeff*( f[2:, 1:-1 , 1:-1 ] - f[:-2,1:-1, 1:-1])/(2*self.__DX)
-        return g
-    
-    def central_diff_1st_3dY (self, f, coeff) :
-        """ Calcula a "derivada centrada" da componente Y de um campo 3D f"""
-        g = coeff*( f[1:-1, 2:, 1:-1 ] - f[1:-1,:-2, 1:-1])/(2*self.__DY)
-        return g
-    
-    def central_diff_1st_3dZ (self, f, coeff) :
-        """ Calcula a "derivada centrada" da componente Z de um campo 3D f"""
-        g = coeff*( f[1:-1, 1:-1,2:] - f[1:-1, 1:-1,:-2])/(2*self.__DZ)
-        return g
-    
-    def central_diff_2nd_3d (self , f, coeff1 , coeff2 ) :
-        """ Calcula "segunda derivada central" de um campo 3D f, retorna g"""
-        g = (coeff1/self.__DX**2*( f [2:,1:-1,1:-1] - 2*f[1:-1,1:-1] + f [:-2,1:-1,1:-1] )
-             + coeff2/self.__DY**2*( f[1:-1,2:, 1:-1] - 2* f[1:-1,1:-1] + f[1:-1,:-2,1:-1])
-             + coeff3/self.__DZ**2*( f[1:-1,1:-1, 2:] - 2* f[1:-1,1:-1] + f[1:-1,1:-1,:-2]))
-        return g
-        
-     
-class The_Fluid_Solver_3D (TheSolver3D):
-    def diffuse_explicit_2d( self , f ) :
-        """viscosidade assumidamente 0, calcula a difusao de um campo 3d por explicita diferenciação central"""
-        return self.__DT*self.central_diff_2nd_3d( f,self.__NU,self.__NU, self.__NU)
-    
-    def diffuse_implicit_3d ( self , f0 , f , diff_coeff ) :
-        """Performs diffusion of a 2D field implicitly ; diff_coef (NU or ETA i s assumed to be constant . """
-        return ( ( f0[1:-1,1:-1] + ( diff_coeff * self.__DT) /( self.__DX**2 * self.__DY**2) 
-                  * ( self.__DY**2 * ( f [ 2 : , 1:-1] + f [ :-2 , 1:-1] )
-                  + self.__DX**2 * ( f [1:-1 , 2 : ] + f [1:-1 , : -2] ) ) )
-                    / (1 + (2*diff_coeff * self.__DT) / ( self.DX**2 * self.__DY**2)
-                  * ( self.__DY**2 + self.__DX**2)))
-    
-    
-    def relax_pressure_poisson( self , p , src ) :
-        """ Resolve a equação de Poisson para campo de pressão 3D por diferenciação central em ambas as dimensões.
-            Resolve a equação de Laplace para um campo de pressão 3D quando src = 0"""
-        p[1:-1,1:-1,1:-1] = ((self.__DY**2*self.__DZ**2*(p[2:,1:-1,1:-1] + p[:-2,1:-1,1:-1] )+ 
-                              self.__DX**2*self.__DZ**2*(p[1:-1,2:,1:-1] + p[1:-1,:-2,1:-1] ) + 
-                              self.__DX**2*self.__DY**2*(p[1:-1,1:-1,2:] + p[1:-1,1:-1,:-2] )
-                               - self.__DX**2 * self.DY**2*self.__DZ**2 * src[1: -1 ,1: -1,1: -1] )/ ( 2*( self.__DX**2 + self.__DY**2+ self.__DZ**2) ) )
-        return p
-    
-    def transform_pressure_poisson ( self , p , src ) :
-        """ Resolve a equação de Poisson para campos de pressão 3D pela Fast Fourier Trasnform (fft).
-            Isto resolve a equação de Laplace  para campos de pressão 3D quando src = 0"""
-        srcTrans = np.fft.fftn( src [1: -1 ,1: -1, 1:-1] )
-        kx, ky, kz = np.meshgrid (np.fft.fftfreq(self.__NX-2, d = self.__DX) , np.fft.fftfreq(self.__NY-2, d = self.__DY) , np.fft.fftfreq(self.__NZ-2, d = self.__DZ) )
-        denom = 1.0/(4 - 2*np.cos(2*np.pi*kx*self.__DX) - 2*np.cos(2*np.pi*ky*self.__DY) - 2*np.cos(2*np.pi*kz*self.__DZ))
-        denom[0, 0 , 0] = 0
-        p[1: -1 ,1: -1 ,1: -1] = np.real_if_close(np.fft.ifftn (-srcTrans * denom * self.__DX * self.__DY) * self.__DZ) )
-        return p
-         """
-        
-            
-        
+            f.close()
+        self.__RHO = np.array(initial_density) # Mass density initial condition
+        self.__U = np.array(initial_u) # Initial velocity in XX
+        self.__V = np.array(initial_v) # Initial velocity in YY
+        self.__W = np.array(initial_w)
+        self.__P = np.array(initial_presure) #Intial Presure matrix
+        self.__F = np.array(initial_Force) #Intial Force matrix
+        self.__MAGNETIC_SOURCES = magnetic_sources
+
+    def calc_Force(self):
+        #Magnetic Component
+        B =  self.__MAGNETIC_SOURCES.get_sources().getB(POS).reshape(self.__NY+2,self.__NX+2,3)
+        Bz = -np.copy(B[:,:,1])
+        B[:,:,1], B[:,:,2] = B[:,:,2], Bz
+        J = func.CURL(B)/CONST.MU_0
+        F_B = np.cross(J,B) 
+        #Pressure Component
+        GRAD_P = np.empty(F_B.shape,float)
+        GRAD_P[:,:,0], GRAD_P[:,:,1] = np.gradient(self.__P)
+        GRAD_P[:,:,2] = np.zeros((self.__NY,self.__NX))
+        self.__F = F_B - GRAD_P
