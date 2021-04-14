c702762b4b0bb8c8d2a3e66dbd7a18882eb9972a
LeanderReascos <49106116+LeanderReascos@users.noreply.github.com>
2021-04-14 15:58:30 +0100

    Add solver, and update MHD

--- Simualtion Model/libs/MHD.py
+++ Simualtion Model/libs/MHD.py
@@ -3,7 +3,7 @@ import magnetic_fields as mf
 import funtions_and_constants as func
 from functions import CONSTANTS as CONST
 
-class Plasma:
+class Plasma(TheSolver):
     '''
         Plasma object that is described as a MHD ideal fluid
     '''
@@ -18,8 +18,8 @@ class Plasma:
                     self.__NX, self.__NY = [int(v) for v in value.split(',')] # Number of points of the box 
                 elif tag == 'Box_Limites':
                     self.__XMIN, self.__XMAX, self.__YMIN, self.__YMAX = [float(v) for v in value.split(',')] # Dimentions of the box
-                    self.__DX = (self.__XMAX-self.__XMIN)/(self.__NX-1)
-                    self.__DY = (self.__YMAX-self.__YMIN)/(self.__NY-1)
+                    DX = (self.__XMAX-self.__XMIN)/(self.__NX-1)
+                    DY = (self.__YMAX-self.__YMIN)/(self.__NY-1)
                     X = np.linspace(self.__XMIN-self.__DX,self.__XMAX+self.__DX,self.__NX+2)
                     Z = np.linspace(self.__YMIN-self.__DY,self.__YMAX+self.__DY,self.__NY+2)
                     self.__POS = np.array([(x,0,z) for z in Z for x in X])
@@ -27,6 +27,7 @@ class Plasma:
                     self.__DT, self.__TF = [float(v) for v in value.split(',')]
                 line = f.readline()
             f.close()
+        super(Plasma, self).__init__(DX,DY)
         self.__RHO = np.array(initial_density) # Mass density initial condition
         self.__U = np.array(initial_u) # Initial velocity in XX
         self.__V = np.array(initial_v) # Initial velocity in YY
@@ -47,3 +48,34 @@ class Plasma:
         GRAD_P[:,:,0], GRAD_P[:,:,1] = np.gradient(self.__P)
         GRAD_P[:,:,2] = np.zeros((self.__NY,self.__NX))
         self.__F = F_B - GRAD_P
+
+    def diffuse_explicit( self , f ) :
+        """viscosidade assumidamente 0, calcula a difusao de um campo 3d por explicita diferenciação central"""
+        return self.__DT*self.central_diff_2nd_3d( f,self.__NU,self.__NU, self.__NU)
+    
+    def diffuse_implicit ( self , f0 , f , diff_coeff ) :
+        """Performs diffusion of a 2D field implicitly ; diff_coef (NU or ETA i s assumed to be constant . """
+        return ( ( f0[1:-1,1:-1] + ( diff_coeff * self.__DT) /( self.__DX**2 * self.__DY**2) 
+                  * ( self.__DY**2 * ( f [ 2 : , 1:-1] + f [ :-2 , 1:-1] )
+                  + self.__DX**2 * ( f [1:-1 , 2 : ] + f [1:-1 , : -2] ) ) )
+                    / (1 + (2*diff_coeff * self.__DT) / ( self.DX**2 * self.__DY**2)
+                  * ( self.__DY**2 + self.__DX**2)))
+    
+    def relax_pressure_poisson( self , p , src ) :
+        """ Resolve a equação de Poisson para campo de pressão 3D por diferenciação central em ambas as dimensões.
+            Resolve a equação de Laplace para um campo de pressão 3D quando src = 0"""
+        p[1:-1,1:-1,1:-1] = ((self.__DY**2*self.__DZ**2*(p[2:,1:-1,1:-1] + p[:-2,1:-1,1:-1] )+ 
+                              self.__DX**2*self.__DZ**2*(p[1:-1,2:,1:-1] + p[1:-1,:-2,1:-1] ) + 
+                              self.__DX**2*self.__DY**2*(p[1:-1,1:-1,2:] + p[1:-1,1:-1,:-2] )
+                               - self.__DX**2 * self.DY**2*self.__DZ**2 * src[1: -1 ,1: -1,1: -1] )/ ( 2*( self.__DX**2 + self.__DY**2+ self.__DZ**2) ) )
+        return p
+    
+    def transform_pressure_poisson ( self , p , src ) :
+        """ Resolve a equação de Poisson para campos de pressão 3D pela Fast Fourier Trasnform (fft).
+            Isto resolve a equação de Laplace  para campos de pressão 3D quando src = 0"""
+        srcTrans = np.fft.fftn( src [1: -1 ,1: -1, 1:-1] )
+        kx, ky, kz = np.meshgrid (np.fft.fftfreq(self.__NX-2, d = self.__DX) , np.fft.fftfreq(self.__NY-2, d = self.__DY) , np.fft.fftfreq(self.__NZ-2, d = self.__DZ) )
+        denom = 1.0/(4 - 2*np.cos(2*np.pi*kx*self.__DX) - 2*np.cos(2*np.pi*ky*self.__DY) - 2*np.cos(2*np.pi*kz*self.__DZ))
+        denom[0, 0 , 0] = 0
+        p[1: -1 ,1: -1 ,1: -1] = np.real_if_close(np.fft.ifftn (-srcTrans * denom * self.__DX * self.__DY) * self.__DZ) )
+        return p
---
+++ Simualtion Model/libs/solver_and_constants.py
@@ -0,0 +1,67 @@
+import numpy as np
+
+class CONSTANTS:
+    def __init__(self):
+        self.MU_0 = 4*np.pi*1e-7
+
+
+class TheSolver:
+    def __init__(self, dx,dy):
+        self.__DX = dx
+        self.__DY = dy
+
+    def back_diff1st (self, f, coeff1 , coeff2, coeff3):
+        """ Calcula a 'derivada para trás' 3D de um campo f, devolvendo como output o campo resultante g"""
+        g = coeff1 ∗(f[ 1:−1 , 1:−1 , 1:−1 ] − f[:−2,1:−1, 1:-1]) / self.__DX
+        g += coeff2 ∗(f[ 1:−1 , 1:−1 , 1:−1 ] − f[ 1:−1 ,:−2, 1:-1]) / self.__DY
+        g += coeff3 ∗(f[ 1:−1 , 1:−1 , 1:−1 ] − f[ 1:−1 , 1:-1,:−2]) / self.__DZ
+        return g
+    
+    def central_diff_implicit_1st (self, f, coeff1, coeff2, coeff3):
+        """ Calcula a 'centrada' 3D de um campo f, devolvendo como output o campo resultante g"""
+        g = coeff1*( f[2:,1:-1,1:-1] + f [:-2,1:-1,1:-1])/(2*self.__DX)
+        g += coeff2*( f[1:-1,2:,1:-1] + f[1:-1,:-2,1:-1])/(2*self.__DY)
+        g += coeff3*( f[1:-1,1:-1,2:] + f[1:-1,1:-1,:-2])/(2*self.__DZ)
+        return g
+    
+    def central_diff_implicit_1st (self, f, coeff1, coeff2, coeff3):
+        """ Calcula a 'centrada' 3D de um campo f, devolvendo como output o campo resultante g"""
+        g = coeff1*( f[2:,1:-1,1:-1] + f [:-2,1:-1,1:-1])/(2*self.__DX)
+        g += coeff2*( f[1:-1,2:,1:-1] + f[1:-1,:-2,1:-1])/(2*self.__DY)
+        g += coeff3*( f[1:-1,1:-1,2:] + f[1:-1,1:-1,:-2])/(2*self.__DZ)
+        return g
+    
+    def central_diff_1st_X (self, f, coeff) :
+        """ Calcula a "derivada centrada" da componente X de um campo 3D f"""
+        g = coeff*( f[2:, 1:-1 , 1:-1 ] - f[:-2,1:-1, 1:-1])/(2*self.__DX)
+        return g
+    
+    def central_diff_1st_Y (self, f, coeff) :
+        """ Calcula a "derivada centrada" da componente Y de um campo 3D f"""
+        g = coeff*( f[1:-1, 2:, 1:-1 ] - f[1:-1,:-2, 1:-1])/(2*self.__DY)
+        return g
+    
+    def central_diff_1st_Z (self, f, coeff) :
+        """ Calcula a "derivada centrada" da componente Z de um campo 3D f"""
+        g = coeff*( f[1:-1, 1:-1,2:] - f[1:-1, 1:-1,:-2])/(2*self.__DZ)
+        return g
+    
+    def central_diff_2nd(self , f, coeff1 , coeff2 ) :
+        """ Calcula "segunda derivada central" de um campo 3D f, retorna g"""
+        g = (coeff1/self.__DX**2*( f [2:,1:-1,1:-1] - 2*f[1:-1,1:-1] + f [:-2,1:-1,1:-1] )
+             + coeff2/self.__DY**2*( f[1:-1,2:, 1:-1] - 2* f[1:-1,1:-1] + f[1:-1,:-2,1:-1])
+             + coeff3/self.__DZ**2*( f[1:-1,1:-1, 2:] - 2* f[1:-1,1:-1] + f[1:-1,1:-1,:-2]))
+        return g
+
+    def CURL(A):
+        ''' CULR of a tensor A that doesn't change in zz axis '''
+        Ax,Ay,Az = A[:,:,0],A[:,:,1],A[:,:,2]
+        rot_x = central_diff_1st_dY(Az, 1)                              # dAz/dy - dAy/dz (0)  
+        rot_y = central_diff_1st_dX(Az, 1)                              # dAx/dz (0) - dAz/dx 
+        rot_z = central_diff_1st_dX(Ay, 1) - central_diff_1st_dY(Ax, 1) # dAy/dx - dAx/dy 
+        ni,nj,_ = A.shape()
+        ROT = np.empty((ni-2,nj-2,3),float)
+        ROT[:,:,0], ROT[:,:,1], ROT[:,:,2] = rot_x, rot_y, rot_z
+    return ROT
+        
+    
