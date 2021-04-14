29b03abfd52b80f0232e83eab66c07bd15a04935
magdamd <49106367+magdamd@users.noreply.github.com>
2021-04-14 22:04:24 +0100

    Update MHD.py

--- Simualtion Model/libs/MHD.py
+++ Simualtion Model/libs/MHD.py
@@ -49,6 +49,92 @@ class Plasma(TheSolver):
         GRAD_P[:,:,0], GRAD_P[:,:,1] = np.gradient(self.__P)
         GRAD_P[:,:,2] = np.zeros((self.__NY,self.__NX))
         self.__F = F_B - GRAD_P
+        
+    def linear_advect_explicit ( self , f ) :
+        """Calcula a advection linear de um campo 2D por diferenciação explícita para trás"""
+        return self.__DT*self.backdiff_1st ( f, self.__C, self.__C)
+
+    def linear_advect_implicit ( self , f , XX,YY) :
+        """Calcula a advection linear de um campo 2D implicitamente mantendo todos os valores limitados pelo domínio do apêndice A do código fonte 80 THE ARGO, por backtracking 
+domain , by c e l l 􀀀c ent e r ed back􀀀t r a c k ing and applying
+ne c e s s a ry we ight s . """
+x = XX 􀀀 ( s e l f .DT/ s e l f .DX  s e l f .C)
+y = YY 􀀀 ( s e l f .DT/ s e l f .DY  s e l f .C)
+x = np . where ( x < 0 . 5 , 0 . 5 , x )
+y = np . where ( y < 0 . 5 , 0 . 5 , y )
+x = np . where ( x > ( s e l f .NX􀀀2) + 0 . 5 ,
+( s e l f .NX􀀀2) + 0 . 5 , x )
+y = np . where ( y > ( s e l f .NY􀀀2) + 0 . 5 ,
+( s e l f .NY􀀀2) + 0 . 5 , y )
+i 0 = x . as type ( int ) ; j 0 = y . as type ( int )
+i 1 = i 0 + 1 ; j 1 = j 0 + 1
+s1 = x 􀀀 i 0 ; t1 = y 􀀀 j 0
+s0 = 1 􀀀 s1 ; t0 = 1 􀀀 t1
+return ( s0  ( t0  f [ i0 , j 0 ] + t1  f [ i0 , j 1 ] )
++ s1  ( t0  f [ i1 , j 0 ] + t1  f [ i1 , j 1 ] ) )
+# end l i n e a r a d v e c t imp l i c i t 2 d
+def l i n e a r a d v e c t imp l i c i t p e r i o d i c 2 d ( s e l f , f , XX,YY) :
+"""Performs l i n e a r adve c t ion o f a 2D f i e l d ( f )
+imp l c i t l y , with p e r i o d i c boundar ies , by c e l l 􀀀c ent e r ed
+back􀀀t r a c k ing and applying ne c e s s a ry we ight s . """
+x = XX 􀀀 ( s e l f .DT/ s e l f .DX  C)
+APPENDIX A. THE ARGO SOURCE CODE 81
+y = YY 􀀀 ( s e l f .DT/ s e l f .DY  C)
+x = x % ( s e l f .NX 􀀀 2)
+y = y % ( s e l f .NY 􀀀 2)
+i 0 = x . as type ( int ) ; j 0 = y . as type ( int )
+i 1 = i 0 + 1 ; j 1 = j 0 + 1
+s1 = x 􀀀 i 0 ; t1 = y 􀀀 j 0
+s0 = 1 􀀀 s1 ; t0 = 1 􀀀 t1
+return ( s0  ( t0  f [ i0 , j 0 ] + t1  f [ i0 , j 1 ] )
++ s1  ( t0  f [ i1 , j 0 ] + t1  f [ i1 , j 1 ] ) )
+# end l i n e a r a d v e c t imp l i c i t p e r i o d i c 2 d
+def n o n l i n e a r a d v e c t e x p l i c i t 2 d ( s e l f , f , fx , fy ) :
+"""Performs nonl ine a r adve c t ion o f 2D f i e l d ( f ) , by 2D
+ve c t o r f i e l d ( fx , fy ) by e x p l i c i t backward d i f f e r e n c i n g .
+"""
+return s e l f .DT s e l f . b a c k d i f f 1 s t 2 d ( f ,
+fx [ 1:􀀀1 , 1:􀀀1 ] , fy [ 1:􀀀1 , 1:􀀀1 ] )
+# end n o n l i n e a r a d v e c t e x p l i c i t 2 d
+def n o n l i n e a r a d v e c t imp l i c i t 2 d ( s e l f , f , fx , fy , XX,YY) :
+"""Performs nonl ine a r adve c t ion o f 2D f i e l d ( f ) by 2D
+ve c t o r f i e l d ( fx , fy ) , ke eping a l l va lue s bounded wi thin
+the domain , by c e l l 􀀀c ent e r ed back􀀀t r a c k ing and
+applying ne c e s s a ry we ight s . """
+APPENDIX A. THE ARGO SOURCE CODE 82
+x = XX 􀀀 ( s e l f .DT/ s e l f .DX  fx [1: 􀀀1 ,1: 􀀀1] )
+y = YY 􀀀 ( s e l f .DT/ s e l f .DY  fy [1: 􀀀1 ,1: 􀀀1] )
+x = np . where ( x < 0 . 5 , 0 . 5 , x )
+y = np . where ( y < 0 . 5 , 0 . 5 , y )
+x = np . where ( x > ( s e l f .NX􀀀2) + 0 . 5 ,
+( s e l f .NX􀀀2) + 0 . 5 , x )
+y = np . where ( y > ( s e l f .NY􀀀2) + 0 . 5 ,
+( s e l f .NY􀀀2) + 0 . 5 , y )
+i 0 = x . as type ( int ) ; j 0 = y . as type ( int )
+i 1 = i 0 + 1 ; j 1 = j 0 + 1
+s1 = x 􀀀 i 0 ; t1 = y 􀀀 j 0
+s0 = 1 􀀀 s1 ; t0 = 1 􀀀 t1
+return ( s0  ( t0  f [ i0 , j 0 ] + t1  f [ i0 , j 1 ] )
++ s1  ( t0  f [ i1 , j 0 ] + t1  f [ i1 , j 1 ] ) )
+# end n o n l i n e a r a d v e c t imp l i c i t 2 d
+def n o n l i n e a r a d v e c t imp l i c i t p e r i o d i c 2 d ( s e l f , f , fx , fy ,
+XX,YY) :
+"""Advection by 2D f i e l d ( fx , fy ) o f any component o f a
+2D f i e l d ( f ) , with p e r i o d i c boundar ies , by c e l l 􀀀
+c ent e r ed back􀀀t r a c k ing and applying ne c e s s a ry we ight s .
+"""
+x = XX 􀀀 ( s e l f .DT/ s e l f .DX  fx [1: 􀀀1 ,1: 􀀀1] )
+APPENDIX A. THE ARGO SOURCE CODE 83
+y = YY 􀀀 ( s e l f .DT/ s e l f .DY  fy [1: 􀀀1 ,1: 􀀀1] )
+x = x % ( s e l f .NX 􀀀 2)
+y = y % ( s e l f .NY 􀀀 2)
+i 0 = x . as type ( int ) ; j 0 = y . as type ( int )
+i 1 = i 0 + 1 ; j 1 = j 0 + 1
+s1 = x 􀀀 i 0 ; t1 = y 􀀀 j 0
+s0 = 1 􀀀 s1 ; t0 = 1 􀀀 t1
+return ( s0  ( t0  f [ i0 , j 0 ] + t1  f [ i0 , j 1 ] )
++ s1  ( t0  f [ i1 , j 0 ] + t1  f [ i1 , j 1 ] ) )
+# end n o n l i n e a r a d v e c t imp l i c i t p e r i o d i c 2 d
 
     def diffuse_explicit( self , f ) :
         """viscosidade assumidamente 0, calcula a difusao de um campo por explicita diferenciação central"""
