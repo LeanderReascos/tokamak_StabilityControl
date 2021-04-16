e2e3fc0c02dbccd445921eeb3d7aabc9e8fdb980
magdamd <49106367+magdamd@users.noreply.github.com>
2021-04-16 12:57:45 +0100

    Update MHD.py

--- Simualtion Model/libs/MHD.py
+++ Simualtion Model/libs/MHD.py
@@ -52,102 +52,82 @@ class Plasma(TheSolver):
         
     def linear_advect_explicit ( self , f ) :
         """Calcula a advection linear de um campo 2D por diferenciação explícita para trás"""
-        return self.__DT*self.backdiff_1st ( f, self.__C, self.__C)
+        return self.__DT*self.back_diff1st ( f, self.__C, self.__C)
 
     def linear_advect_implicit ( self , f , XX,YY) :
-        """Calcula a advection linear de um campo 2D implicitamente mantendo todos os valores limitados pelo domínio do apêndice A do código fonte 80 THE ARGO, por backtracking 
-domain , by c e l l 􀀀c ent e r ed back􀀀t r a c k ing and applying
-ne c e s s a ry we ight s . """
-x = XX 􀀀 ( s e l f .DT/ s e l f .DX  s e l f .C)
-y = YY 􀀀 ( s e l f .DT/ s e l f .DY  s e l f .C)
-x = np . where ( x < 0 . 5 , 0 . 5 , x )
-y = np . where ( y < 0 . 5 , 0 . 5 , y )
-x = np . where ( x > ( s e l f .NX􀀀2) + 0 . 5 ,
-( s e l f .NX􀀀2) + 0 . 5 , x )
-y = np . where ( y > ( s e l f .NY􀀀2) + 0 . 5 ,
-( s e l f .NY􀀀2) + 0 . 5 , y )
-i 0 = x . as type ( int ) ; j 0 = y . as type ( int )
-i 1 = i 0 + 1 ; j 1 = j 0 + 1
-s1 = x 􀀀 i 0 ; t1 = y 􀀀 j 0
-s0 = 1 􀀀 s1 ; t0 = 1 􀀀 t1
-return ( s0  ( t0  f [ i0 , j 0 ] + t1  f [ i0 , j 1 ] )
-+ s1  ( t0  f [ i1 , j 0 ] + t1  f [ i1 , j 1 ] ) )
-# end l i n e a r a d v e c t imp l i c i t 2 d
-def l i n e a r a d v e c t imp l i c i t p e r i o d i c 2 d ( s e l f , f , XX,YY) :
-"""Performs l i n e a r adve c t ion o f a 2D f i e l d ( f )
-imp l c i t l y , with p e r i o d i c boundar ies , by c e l l 􀀀c ent e r ed
-back􀀀t r a c k ing and applying ne c e s s a ry we ight s . """
-x = XX 􀀀 ( s e l f .DT/ s e l f .DX  C)
-APPENDIX A. THE ARGO SOURCE CODE 81
-y = YY 􀀀 ( s e l f .DT/ s e l f .DY  C)
-x = x % ( s e l f .NX 􀀀 2)
-y = y % ( s e l f .NY 􀀀 2)
-i 0 = x . as type ( int ) ; j 0 = y . as type ( int )
-i 1 = i 0 + 1 ; j 1 = j 0 + 1
-s1 = x 􀀀 i 0 ; t1 = y 􀀀 j 0
-s0 = 1 􀀀 s1 ; t0 = 1 􀀀 t1
-return ( s0  ( t0  f [ i0 , j 0 ] + t1  f [ i0 , j 1 ] )
-+ s1  ( t0  f [ i1 , j 0 ] + t1  f [ i1 , j 1 ] ) )
-# end l i n e a r a d v e c t imp l i c i t p e r i o d i c 2 d
-def n o n l i n e a r a d v e c t e x p l i c i t 2 d ( s e l f , f , fx , fy ) :
-"""Performs nonl ine a r adve c t ion o f 2D f i e l d ( f ) , by 2D
-ve c t o r f i e l d ( fx , fy ) by e x p l i c i t backward d i f f e r e n c i n g .
-"""
-return s e l f .DT s e l f . b a c k d i f f 1 s t 2 d ( f ,
-fx [ 1:􀀀1 , 1:􀀀1 ] , fy [ 1:􀀀1 , 1:􀀀1 ] )
-# end n o n l i n e a r a d v e c t e x p l i c i t 2 d
-def n o n l i n e a r a d v e c t imp l i c i t 2 d ( s e l f , f , fx , fy , XX,YY) :
-"""Performs nonl ine a r adve c t ion o f 2D f i e l d ( f ) by 2D
-ve c t o r f i e l d ( fx , fy ) , ke eping a l l va lue s bounded wi thin
-the domain , by c e l l 􀀀c ent e r ed back􀀀t r a c k ing and
-applying ne c e s s a ry we ight s . """
-APPENDIX A. THE ARGO SOURCE CODE 82
-x = XX 􀀀 ( s e l f .DT/ s e l f .DX  fx [1: 􀀀1 ,1: 􀀀1] )
-y = YY 􀀀 ( s e l f .DT/ s e l f .DY  fy [1: 􀀀1 ,1: 􀀀1] )
-x = np . where ( x < 0 . 5 , 0 . 5 , x )
-y = np . where ( y < 0 . 5 , 0 . 5 , y )
-x = np . where ( x > ( s e l f .NX􀀀2) + 0 . 5 ,
-( s e l f .NX􀀀2) + 0 . 5 , x )
-y = np . where ( y > ( s e l f .NY􀀀2) + 0 . 5 ,
-( s e l f .NY􀀀2) + 0 . 5 , y )
-i 0 = x . as type ( int ) ; j 0 = y . as type ( int )
-i 1 = i 0 + 1 ; j 1 = j 0 + 1
-s1 = x 􀀀 i 0 ; t1 = y 􀀀 j 0
-s0 = 1 􀀀 s1 ; t0 = 1 􀀀 t1
-return ( s0  ( t0  f [ i0 , j 0 ] + t1  f [ i0 , j 1 ] )
-+ s1  ( t0  f [ i1 , j 0 ] + t1  f [ i1 , j 1 ] ) )
-# end n o n l i n e a r a d v e c t imp l i c i t 2 d
-def n o n l i n e a r a d v e c t imp l i c i t p e r i o d i c 2 d ( s e l f , f , fx , fy ,
-XX,YY) :
-"""Advection by 2D f i e l d ( fx , fy ) o f any component o f a
-2D f i e l d ( f ) , with p e r i o d i c boundar ies , by c e l l 􀀀
-c ent e r ed back􀀀t r a c k ing and applying ne c e s s a ry we ight s .
-"""
-x = XX 􀀀 ( s e l f .DT/ s e l f .DX  fx [1: 􀀀1 ,1: 􀀀1] )
-APPENDIX A. THE ARGO SOURCE CODE 83
-y = YY 􀀀 ( s e l f .DT/ s e l f .DY  fy [1: 􀀀1 ,1: 􀀀1] )
-x = x % ( s e l f .NX 􀀀 2)
-y = y % ( s e l f .NY 􀀀 2)
-i 0 = x . as type ( int ) ; j 0 = y . as type ( int )
-i 1 = i 0 + 1 ; j 1 = j 0 + 1
-s1 = x 􀀀 i 0 ; t1 = y 􀀀 j 0
-s0 = 1 􀀀 s1 ; t0 = 1 􀀀 t1
-return ( s0  ( t0  f [ i0 , j 0 ] + t1  f [ i0 , j 1 ] )
-+ s1  ( t0  f [ i1 , j 0 ] + t1  f [ i1 , j 1 ] ) )
-# end n o n l i n e a r a d v e c t imp l i c i t p e r i o d i c 2 d
+        """Calcula a advection linear de um campo 2D implicitamente mantendo todos os valores limitados pelo domínio, por backtracking centrado na célula e aplicando pesos necessários"""
+        x = XX - ( self.__DT/self.__DX * self.__C)
+        y = YY - ( self.__DT/ self.__DY * self.__C)
+        x = np.where ( x < 0.5 , 0.5 , x )
+        y = np.where ( y < 0.5 , 0.5 , y )
+        x = np.where ( x > ( self.__NX-2) + 0.5 ,( self.__NX-2) + 0.5 , x )
+        y = np . where ( y > ( self.__NY-2) + 0.5 ,( self.__NY-2) + 0.5 , y )
+        i0 = x.astype(int) ; j0 = y.astype(int)
+        i1 = i0 + 1 ; j1 = j0 + 1
+        s1 = x - i0 ; t1 = y - j0
+        s0 = 1 - s1 ; t0 = 1 - t1
+        return ( s0*(t0*f[i0,j0] + t1*f[i0,j1])+ s1*(t0*f[i1,j0] + t1*f[i1,j1]))
+
+
+    def linear_advect_implicit_periodic( self, f, XX ,YY) :
+        """Calcula advection linear de um campo 2D (f) implicitamente com limites periódicos por backtracking centrado na célula aplicando os pesos necessários"""
+        x = XX - ( self.__DT/ self.__DX * C)
+        y = YY - ( self.__DT/ self.__DY * C)
+        x = x % ( self.__NX - 2)
+        y = y % ( self.__NY - 2)
+        i0 = x.astype(int) ; j0 = y.astype(int)
+        i1 = i0 + 1 ; j1 = j0 + 1
+        s1 = x - i0 ; t1 = y - j0
+        s0 = 1 - s1 ; t0 = 1 - t1
+        return ( s0 * ( t0 * f [i0, j0] + t1 * f[i0,j1]) + s1*(t0*f[i1,j0] + t1*f[i1,j1]))
+
+
+    def non_linear_advect_explicit( self, f, fx, fy) :
+        """Calcula a advection não linear de um campo 2D (f) por um campo vetorial 2D (fx, fy) por explicito backward diferenciação"""
+        return self.__DT*self.back_diff1st( f, fx[1:-1,1:-1], fy[1:-1,1:-1])
+
+
+
+    def non_linear_advect_implicit ( self, f, fx, fy , XX,YY) :
+        """Calcula advection nao linear de um campo 2D (f) por um campo vectorial (fx, fy), mantendo os valores limitados no domínio, através de backtracking centrado na célula e aplicando pesos necessários"""
+        x = XX - ( self.__DT/ self.__DX * fx[1:-1,1:-1] )
+        y = YY - ( self.__DT/ self.__DY * fy[1:-1,1:-1] )
+        x = np.where ( x < 0.5 , 0.5 , x )
+        y = np.where ( y < 0 . 5 , 0 . 5 , y )
+        x = np.where ( x > ( self.__NX-2) + 0 . 5 ,(self.__NX-2) + 0.5 , x )
+        y = np.where ( y > ( self.NY-2) + 0.5 ,( self.__NY-2) + 0.5 , y )
+        i0 = x.astype(int); j0 = y.astype(int)
+        i1 = i0 + 1; j1 = j0 + 1
+        s1 = x - i0 ; t1 = y - j0
+        s0 = 1 - s1 ; t0 = 1 - t1
+        return ( s0 * ( t0 * f[i0,j0] + t1 * f[i0,j1])+ s1 * (t0*f[i1,j0] + t1*f[i1,j1]))
+
+
+    def non_linear_advect_implicit_periodic ( self, f, fx, fy,XX,YY) :
+        """Advection de um campo 2D (fx, fy) de qualquer componente de um campo 2D (f) com limites periódicos através de backtracking centrada na célula e aplicando pesos necessários""""
+        x = XX - ( self.__DT/ self.__DX * fx [1:-1 ,1:-1] )
+        y = YY - ( self.__DT/ self.__DY * fy [1:-1 ,1:-1] )
+        x = x % ( self.__NX - 2)
+        y = y % ( self.__NY - 2)
+        i0 = x.astype(int); j0 = y.astype(int)
+        i1 = i0 + 1; j1 = j0 + 1
+        s1 = x - i0 ; t1 = y - j0
+        s0 = 1 - s1 ; t0 = 1 - t1
+        return ( s0 * ( t0 * f[i0,j0] + t1 * f[i0,j1]) + s1 * ( t0 * f[i1,j0] + t1 * f[i1,j1]))
+
 
     def diffuse_explicit( self , f ) :
         """viscosidade assumidamente 0, calcula a difusao de um campo por explicita diferenciação central"""
         return self.__DT*self.central_diff_2nd( f,self.__NU,self.__NU, self.__NU)
-    
-    def diffuse_implicit ( self , f0 , f , diff_coeff ) :
-        """Performs diffusion of a field implicitly ; diff_coef (NU or ETA is assumed to be constant . """
+
+    def diffuse_implicit ( self , f0, f, diff_coeff ) :
+        """Calcula difusão de um campo implicitamente;diff_coef (NU or ETA is assumed to be constant."""
         return ( ( f0[1:-1,1:-1] + ( diff_coeff * self.__DT) /( self.__DX**2 * self.__DY**2) 
                   * ( self.__DY**2 * ( f [2:,1:-1] + f [:-2,1:-1] )
                   + self.__DX**2 * ( f [1:-1,2:] + f [1:-1,:-2] ) ) )
                     / (1 + (2*diff_coeff * self.__DT) / ( self.DX**2 * self.__DY**2)
                   * ( self.__DY**2 + self.__DX**2)))
-      
+
     def relax_pressure_poisson( self , p , src ) :
         """ Resolve a equação de Poisson para campo de pressão 2D por diferenciação central em ambas as dimensões.
             Resolve a equação de Laplace para um campo de pressão 2D quando src = 0"""
@@ -155,7 +135,7 @@ return ( s0  ( t0  f [ i0 , j 0 ] + t1  f [ i0 , j 1 ] )
                               self.__DX**2*(p[1:-1,2:] + p[1:-1,:-2] ) + 
                               - self.__DX**2 * self.DY**2*src[1: -1 ,1: -1] )/ ( 2*( self.__DX**2 + self.__DY**2) ) )
         return p
-    
+
     def transform_pressure_poisson(self, p, src):
         """ Resolve a equação de Poisson para campos de pressão 2D pela Fast Fourier Transform (fft).
             Isto resolve a equação de Laplace  para campos de pressão 2D quando src = 0"""
